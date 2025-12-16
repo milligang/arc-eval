9035c51525d26040f403868579f496e892aa2807
milligang <milligan.grinstead@gmail.com>
2025-12-16 16:55:06 -0600

    updated analysis to loop

--- generators/analysis.py
+++ generators/analysis.py
@@ -1,28 +1,22 @@
 import numpy as np
 from file import get_predictions, get_arctask, find_line_by_uid
 from arc.types import ArcIOPair
-from agents import corrupt_grid
+from agents import corrupt_grid, cmp_grids
 
-def hamming(arr1, arr2):
-    if arr1.shape != arr2.shape:
-        raise ValueError("Arrays must have the same shape.")
-    return np.sum(arr1 != arr2)
+# def hamming(arr1, arr2):
+#     if arr1.shape != arr2.shape:
+#         raise ValueError("Arrays must have the same shape.")
+#     return np.sum(arr1 != arr2)
 
-task_id, ans = get_predictions("1crtxn18", "g25f1")
-last_ans = ans[-1]
-task_line = find_line_by_uid(task_id)
-task = get_arctask(task_line)
+for j in range(0, 10):
+    if (j == 2): continue
+    task_id, ans = get_predictions(f"5pcrtxn{j}", "g25f0")
+    last_ans = ans[-1]
+    task_line = find_line_by_uid(task_id)
+    task = get_arctask(task_line)
 
-print(len(ans))
-# ham = hamming(last_ans, task.test_pairs[0].y)
-ham = 1
-if ham != 0:
-    print(ham)
-    # for i, pair in enumerate(task.train_pairs, start=1):
-    #     pair.plot(show=True, title=f"Demo {i}")
-
-    for i, pair in enumerate(task.test_pairs, start=1):
-        pair.plot(show=True, title=f"Test {i}")
-    
-    prediction = ArcIOPair(task.test_pairs[0].y, last_ans)
-    prediction.plot(show=True)
+    if not cmp_grids(last_ans, task.test_pairs[0].y):        
+        prediction = ArcIOPair(task.test_pairs[0].y, last_ans)
+        prediction.plot(show=True, title=f"Task {j}")
+    else:
+        print(j)
